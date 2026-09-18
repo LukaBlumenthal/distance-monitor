fce28737da7db7dec728f8e80bc6972dc1430d77
Luka <lukablumenthal@aol.com>
2026-09-18 12:41:46 +0200

    Add simulated sensor values

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ with open("sensor_data.csv", "r") as file:
 
     for row in reader:
         distance = int(row["distance"])
-        print(f"Distance: {distance} cm")
+        
+        if distance < 20:
+           status = "STOP"
+        elif distance <= 50:
+           status = "WARNING"
+        else:
+           status = "OK"
+
+        print(f"Distance: {distance} cm | Status: {status}")
